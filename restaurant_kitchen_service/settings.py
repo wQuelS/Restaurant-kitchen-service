87f2e8ec27eb447e7a1c004c2c0ee0ff83a85393
Alex Parkhomenko <alex.wquels@gmail.com>
2022-12-07 10:11:55 +0200

    Fixed security issue

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-SECRET_KEY = "5r^@tbl#c11de9btdw4s%*v1)3)z!*r79v1^kwy4so)q!=3$yl"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-service-rlyr.onrender.com"]
 
